e5a8f9eea809f7d6d41d919c2fae9d5378638f5e
kchantha <thasmile1@gmail.com>
2022-07-06 17:22:22 +0700

    upload portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import Projects from './Projects';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class RegularClass{}
+// class ComponentClass extends Component{}
+
+// const regularClassInsance = new RegularClass();
+// const componentClassInstance = new ComponentClass();
+
+// console.log('regularClassInsance',regularClassInsance);
+// console.log('componentClassInstance',componentClassInstance);
+
+
+class App extends Component {
+    state={displayBio: false}; //declare state
+
+    toggleDisplayBio=() =>{ //declare method
+        this.setState({displayBio: !this.state.displayBio});
+    }
+    render(){
+        return(
+            <div>
+                <h1>Hello</h1>
+                <p> My name is Jhon</p>
+                <p>I'm looking for someone</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <h1>I live in Cambodia</h1>
+                            <p> My favorite</p>
+                            <p>Bside coding</p>
+                            <button onClick={this.toggleDisplayBio}>Show Less</button>
+                        </div>
+                    ) : 
+                    <div>
+                        <button onClick={this.toggleDisplayBio}>Show More</button>
+                    </div>
+                }
+                <hr/>
+                <Projects/>
+            </div>
+            
+            
+        )
+        
+    }
 }
 
 export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+//Array object
+const PROJECT =[
+    {
+        id: 1,
+        title: 'Example React Application',
+        description: 'A React App that I build, involving JS and core web dev concepts!',
+        link: 'https://github.com/15Dkatz/exple',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'My API',
+        description: 'A REST API that I build from scratch with GET and POST requests!',
+        link: 'https://github.com/15Dkatz/exple',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Operating System Final Project',
+        description: 'My unique final project for my university Operating System course.',
+        link: 'https://github.com/15Dkatz/exple',
+        image: project3
+    }
+];
+
+export default PROJECT; 
